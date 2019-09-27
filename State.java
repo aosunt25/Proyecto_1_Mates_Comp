acf19cd7e73f689036c189aff3609bc8bfd70d9f
Ed9ar <a01339939@itesm.mx>
2019-09-27 00:14:45 -0500

    Add files via upload

---
+++ State.java
@@ -0,0 +1,49 @@
+//package estados;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List; 
+
+public class State<T>{
+    public List<Trans<T>> trans;
+    private T element;
+    private String type;
+    private boolean visited;
+    //private State<String> neighbour;
+
+    public State(T element) {
+		this.element = element;
+		trans= new ArrayList<>();
+	}
+
+    public void setType(String type){
+        this.type = type;
+    }
+    public String getType(){
+        return type;
+    }
+
+    public T getElement() {
+		return element;
+	}
+	public void setElement(T element) {
+		this.element = element;
+    }
+    
+	public List<Trans<T>> getTrans() {
+		return trans;
+	}
+	public void setTrans(List<Trans<T>> trans) {
+		this.trans = trans;
+	}
+	public void addTrans(Trans<T> tr) {
+		trans.add(tr);
+    }
+    
+	public boolean isVisited() {
+		return visited;
+	}
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+}		
+
