3c5bd6cc6591b91fbdd1b274c808861fe064b248
Alfredo Osuna <alfredoosuna40@gmail.com>
2019-09-26 21:26:13 -0500

    Test 2
    Agregue a la carpeta el archivo de test 2

--- Main.java
+++ Main.java
@@ -5,13 +5,14 @@ import estados.*;
 
 class Main{
     public static void main(String[] args){
-        
-        File archivo = new File ("test1.txt");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe el nombre del Archivo");
+        String nombreArchivo = scanner.nextLine() + ".txt";
+        File archivo = new File (nombreArchivo);
         System.out.println(archivo.exists());
         ArrayList cadenaStados = new ArrayList<String>();
         ArrayList cadenaAlfabeto = new ArrayList<String>();
         cadenaAlfabeto.add("lmd");
-        Scanner scanner;
         try{    
             scanner = new Scanner(archivo);
             int numDeLinea=1;
