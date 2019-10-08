54a37e7c5a11d8e6c46faabf35a2b4e9d9d17849
Ed9ar <a01339939@itesm.mx>
2019-10-08 18:27:27 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,20 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-//import estados.*;
 
 class Main{
     public static void main(String[] args){
-        
+        Ndfa<String> ndfa = new Ndfa<>();
+        String strn;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Write the file's name");
         String nombreArchivo = scanner.nextLine() + ".txt";
         File archivo = new File (nombreArchivo);
         System.out.println(archivo.exists());
-        ArrayList cadenaStados = new ArrayList<State>();
-
-        Ndfa<String> ndfa = new Ndfa<>();
+        
         ArrayList cadenaAlfabeto = new ArrayList<String>();
+        ArrayList language = new ArrayList<String>();
+
         cadenaAlfabeto.add("lmd");
         try{    
             scanner = new Scanner(archivo);
@@ -54,53 +54,29 @@ class Main{
             e.printStackTrace();
         }
 
-       
-        
-        ndfa.addState("q0", "ini");
-        ndfa.addState("q1", "fin");
-        ndfa.addTrans("q0", "q0", 'l');
-        ndfa.addTrans("q0", "q0", 'a');
-        ndfa.addTrans("q0", "q1", 'b');
-        ndfa.addTrans("q1", "q1", 'b');
-        ndfa.addTrans("q1", "q0", 'l');
 
 
         System.out.println("Language");
+        System.out.println(" ");
         System.out.println(cadenaAlfabeto);
 
-        String str = "abab";
-        StrProcessing process = new StrProcessing<>(ndfa);
-
-
-        System.out.println("This are the states with their transitions");
-        for(int i = 0; i < ndfa.getStates().size();i++){
-            System.out.print(ndfa.getStates().get(i).getElement());
-            System.out.println(ndfa.getStates().get(i).getType());
-           
-            for(int j = 0; j < ndfa.getStates().get(i).getTrans().size();j++){
-                if(ndfa.getStates().get(i).getTrans().get(j).getQ1().getElement() ==  ndfa.getStates().get(i).getElement()){
-                    System.out.println("Going to " + ndfa.getStates().get(i).getTrans().get(j).getQ2().getElement() + " processing " + ndfa.getStates().get(i).getTrans().get(j).getSymbol()+ " " );
-                    
-                }
-                else{
-                    System.out.println("Coming from " + ndfa.getStates().get(i).getTrans().get(j).getQ1().getElement() + " processing " + ndfa.getStates().get(i).getTrans().get(j).getSymbol()+ " " );
-                }
-                
-            }
-            
-        }
-        System.out.println(" ");
-        System.out.println(" ");
-        System.out.println(" ");
-
-        if(process.stringProcessing(str, str.length(), cadenaAlfabeto)){
-            System.out.println("The string is accepted by the language as the intersection with the final state is not empty");
-        }
-        else{
-            System.out.println("The string is not accepted by the language as the intersection with the final state is empty");;
-        }
-        System.out.println(process.reachableStates);
+        language = cadenaAlfabeto;
         
+        ndfa.addState("q0");
+        ndfa.addState("q1");
+        ndfa.addTrans("q0", "lmd", "q0");
+        ndfa.addTrans("q0", "a", "q0");
+        ndfa.addTrans("q0", "b", "q1");
+        ndfa.addTrans("q1", "lmd", "q1");
+        ndfa.addTrans("q1", "lmd", "q0");
+        ndfa.addTrans("q1", "b", "q1");
 
+        ndfa.addTrans("q1", "a", "q0");
+
+        strn = "ababa";
+
+        System.out.println(ndfa.transitionTable);
+        
 }
-}   
+}
+   
--- Ndfa.java
+++ Ndfa.java
@@ -1,67 +1,33 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
+
+import java.util.*;
 
 
-//import estados.*;
-public class Ndfa<T extends Comparable<T>>{
-
-	private List<State<T>> states;
-	private Stack<State<T>> stack;
+public class Ndfa<T extends Comparable>{
+    public HashMap<String, HashMap<String,ArrayList<String>>> transitionTable = new HashMap<String, HashMap<String,ArrayList<String>>>();
 
     public Ndfa(){
-		states = new ArrayList<>();
-		stack= new Stack<>();
+        
     }
 
-    public void addState(T qi, String type){
-        boolean visited = false;
-        for(State<T> state : states){
-            if(state.getElement().compareTo(qi)==0){
-                visited=true;
-            }
-        }
-        if(!visited){
-            states.add(new State<>(qi, type));
-        }
+    public void addState(String llave){
+        transitionTable.put(llave, new HashMap<String, ArrayList<String>>());
     }
 
-	public List<State<T>> getStates(){
-		return states;
-	}
+    public void addTrans(String state1,String symbol , String state2 ){
+        if(transitionTable.get(state1).get(symbol) == null){
+            transitionTable.get(state1).put(symbol, new ArrayList<>());
+        }
+        
 
-
-    public void addTrans(T q1, T q2, char symbol) {
-		State<T> temp1= null;
-		State<T> temp2= null;
-		for(State <T> state: states) {
-			if(state.getElement().compareTo(q1)==0) {
-				temp1= state;
-			}
-		}
-        for(State <T> state: states) {
-			if(state.getElement().compareTo(q2)==0) {
-				temp2= state;
-			}
-		}
-		if(temp1!=null&&temp2!=null) {
-			Trans<T> tr= new Trans<>(temp1,temp2,symbol);
-			temp1.addTrans(tr);
-			temp2.addTrans(tr);
-		}
-	}
+        transitionTable.get(state1).get(symbol).add(state2);
+        System.out.println(transitionTable.get(state1).get(symbol));
+    }
     
-    public void rein() {
-		for(State<T> state:states) {
-			state.setVisited(false);
-		}
-	}
 
+    public void stringProcessing(String str){
 
-	
+    }
 }
- 
