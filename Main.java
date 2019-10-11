fe45153ee0bc336bc15628c95b6bf82dc5953ff5
Ed9ar <a01339939@itesm.mx>
2019-10-10 21:37:22 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,102 +1,320 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import javax.sound.midi.Receiver;
+import java.util.*;
 
-class Main{
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import java.awt.geom.Line2D;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.*;
+import javafx.scene.input.*;
+import javafx.event.*;
+import javafx.beans.value.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Group; 
+import javafx.scene.shape.Circle; 
+import javafx.scene.shape.Line; 
+import javafx.scene.text.*;
+import javafx.scene.paint.Color;
+import javafx.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List; 
+import java.util.Random;
+import java.io.*;
+
+/**
+ * @author Alfredo Osuna Torres
+ * @author Edgar Lopez Valdez
+ */
+
+public class Main extends Application {
+    Scanner scanner = new Scanner(System.in);
+    Label label;
+    Circle circle;
+    VBox vbox = new VBox();
+    VBox vbox2 = new VBox();
+    HBox hbox = new HBox();
+	Group group;
+    Line line;
+    Ndfa<String> ndfa = new Ndfa();
+    String strn;
+    Text elemento;
+    TextField fileText = new TextField();
+    TextField fileText2 = new TextField();
+    int Circlex = 100;
+    int Circley = 100;
+    Pane pane = new Pane();
+    String initState = null;
+    ArrayList alphabetArr = new ArrayList<String>();
+    ArrayList <String> reachable = new ArrayList<String>();
+    ArrayList language = new ArrayList<String>();
+    ArrayList <String> finalState = new ArrayList<String>();
+    String continuar;
+    Button button1 = new Button("Submit");
+    Button button3 = new Button("Clear");
+
+    Button button2 = new Button("Validate");
+    /**
+     * Main method
+     * @param 
+     */
     public static void main(String[] args){
-        Ndfa<String> ndfa = new Ndfa<>();
+        launch(args);
+}
+
+   
+    @Override
+    public void start(Stage primaryStage) {
+        
+        primaryStage.setTitle("NDFA");
+
+        
+        fileText.setPromptText("Write the file's name");
+        fileText.setPrefColumnCount(10);
+        
+        
+        fileText2.setPromptText("Write string");
+        fileText2.setPrefColumnCount(10);
+        
+        vbox2.getChildren().addAll(fileText2,button2);
+        Pane pane = new Pane();
+	    Scene scene= new Scene(pane,1000,1000);
+        vbox.getChildren().addAll(fileText, button1);
+        hbox.getChildren().addAll(vbox,vbox2);
+        pane.getChildren().addAll(hbox);
+        //pane.add(fileText,5,5,5,5);
+        
+        button1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //Ndfa<String> ndfa = new Ndfa<>();
+
+        int index = 0;
         String strn;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Write the file's name");
-        String nombreArchivo = scanner.nextLine() + ".txt";
+        Object val;
+        boolean appear = false;
+        
+        //Scanner scanner = new Scanner(System.in);
+       /**
+        * Asks the user to write the name of the file
+        * Add .txt to the string 
+        */
+        
+        String nombreArchivo = fileText.getText() + ".txt";
         File archivo = new File (nombreArchivo);
         System.out.println(archivo.exists());
         
-        ArrayList cadenaAlfabeto = new ArrayList<String>();
-        ArrayList language = new ArrayList<String>();
+        
+        
 
-        cadenaAlfabeto.add("lmd");
+        alphabetArr.add("lmd");
         try{    
             scanner = new Scanner(archivo);
             int numDeLinea=1;
             while(scanner.hasNextLine()){
                 String linea = scanner.nextLine();
                 Scanner delimitar = new Scanner(linea);
-                delimitar.useDelimiter("\\s*,\\s*");
-                while(delimitar.hasNext()){
-                    switch (numDeLinea){
-                        case 1:
-                        String l = delimitar.next();
-                        //ndfa.addState(l);
-                        //State s = new State(l);
-                        //cadenaStados.add(s);
-                        break;  
-                        case 2:
-                        cadenaAlfabeto.add(delimitar.next());
-                        break;
-                        case 3:
-                        String s1 = delimitar.next();
-                        break;
-                        default:
-                        System.out.println(delimitar.next());
-        
-                        
-                        break;  
+                /**
+                 * Changes case each line of the file 
+                 * After line 4 the file shows all the transactions
+                 */
+                switch (numDeLinea){
+                    /**
+                     * Adds all the states of the NDFA
+                     */
+                    case 1:
+                        delimitar.useDelimiter("\\s*,|=>\\s*");
+                        while(delimitar.hasNext()){
+                            String l = delimitar.next();
+                            /**
+                             * @see Ndfa#addState
+                             * @see Ndfa#addTrans
+                             */
+                            ndfa.addState(l);
+                            ndfa.addTrans(l, "lmd", l);
+                        }
+                    break; 
+                    /**
+                     * Adds the alphabet of the language 
+                     */ 
+                    case 2:
+                        delimitar.useDelimiter("\\s*,|=>\\s*");
+                        while(delimitar.hasNext()){
+                            alphabetArr.add(delimitar.next());
+                        }
+                    break;
+                    /**
+                     * Adds the initial state of the NDFA
+                     */
+                    case 3:
+                        delimitar.useDelimiter("\\s*,|=>\\s*");
+                        while(delimitar.hasNext()){
+                            initState= delimitar.next();
+                        }
+                    break;
+                    /**
+                     * Adds all the final states of the NDFA
+                     */
+                    case 4:
+                        delimitar.useDelimiter("\\s*,|=>\\s*");
+                        while(delimitar.hasNext()){
+                            finalState.add(delimitar.next());
+                        }
+                    break;
+                    /**
+                     * Adds the transactions to the corresponding key
+                     */
+                    default:
+                        ArrayList <String> arrStr = new ArrayList<String>();
+                        int i=0;
+                        delimitar.useDelimiter("\\s*,|=>\\s*");
+                        /**
+                         * Adds all the importante information in arrStr
+                         * arrStr[0] is the initial state of the transaction
+                         * arrStr[1] is the symbol of the stransaction
+                         * arrStr[2] to arrStr[j] are the final states  after the transaction happens 
+                         */
+                        while(delimitar.hasNext()){
+                            arrStr.add(delimitar.next());
+                        i++;
+                        }
+                        /**
+                        * This for add all the transaction in the HashMap
+                        */
+                        for(int j = 2; j<arrStr.size();j++){
+                            String s =arrStr.get(j);
+                            if((arrStr.get(1).contains("lmd")) && arrStr.get(0).contains(s)){
+                            }
+                            else{
+                                /**
+                                * @see Ndfa#addTrans
+                                */
+                                ndfa.addTrans(arrStr.get(0),arrStr.get(1),arrStr.get(j));
+                            }
+                                
+                        }
+                    break;  
                     }
-                    
-                }
                 numDeLinea++;
             }
 
             scanner.close();
+            System.out.println(ndfa.transitionTable);
         }
+        /**
+         * Catch the exception if the file does not exist
+         */
         catch (FileNotFoundException e){
             e.printStackTrace();
         }
 
 
-
-        System.out.println("Language");
+        /**
+         * Prints all the specifications of the Language
+         */
+        System.out.println("Alphabet");
+        System.out.println(alphabetArr);
         System.out.println(" ");
-        System.out.println(cadenaAlfabeto);
-
-        language = cadenaAlfabeto;
+        System.out.println("Initial State");
+        System.err.println(initState);
+        System.out.println(" ");
+        System.out.println("Final State");
+        System.out.println(finalState);
+        System.out.println(" ");
+       
+       
+     
+        /**
+         * This loop will continue until the user does not want to 
+         * try any more string in the language 
+         */
         
-        ndfa.addState("q0");
-        ndfa.addState("q1");
-        ndfa.addTrans("q0", "lmd", "q0");
-        ndfa.addTrans("q0", "a", "q1");
-        ndfa.addTrans("q1", "lmd", "q1");
-        ndfa.addTrans("q1", "b", "q1");
+            }   
+        });
 
-        strn = "ababa";
-        /*
-        
-        System.out.println(strn);
-        if(strn.length()>0){
-            for(int i = 0; i < strn.length(); i++){
-          
-                for(int j = 0; j < language.size();j++){
-                    if(strn.charAt(i) == language.get(j).toString().charAt(0)){
-                        accepted = true;
-                        break;
-                    }
-                    else{
-                        accepted = false;
-                    }
+        button2.addEventHandler(MouseEvent.MOUSE_CLICKED ,new EventHandler<MouseEvent>(){
+			public void handle(MouseEvent e){
+                System.out.println(" ");
+                language = alphabetArr;
+                String continuar = "n";
+                /**
+                 * Asks the user the string to be analyzed
+                 */
+                Scanner sn = new Scanner(System.in);
+                System.out.println("Write the string: ");
+                strn = fileText2.getText();
+                /**
+                 * @see Ndfa#stringProcessing
+                 */
+                reachable = ndfa.stringProcessing(strn, initState);
+                boolean stringAcc = false;
+                /**
+                 * It checks if any states in reachable are final states
+                 */
+                for( int i = 0 ; i<reachable.size() && !stringAcc ;i++){
+                    if(finalState.contains(reachable.get(i))){
+                        stringAcc=true;   
+                    }  
                 }
-            }
-        }
+                if (stringAcc) {
+                    System.out.println("The String: "+strn+" is accepted by the language");
+                    
+                } else {
+                    System.out.println("The String: "+strn+" is not accepted by the language");
+                }
+    
+                /**
+                 * Asks if the user wants to analys another string 
+                 */
+                
+                continuar = sn.nextLine().toLowerCase();
+                System.out.println(fileText.getText());
 
-        if(accepted){
-            break;
-        }
-        else{
-            System.out.println("There are elements in the string that do not belong to this alphabet");
-        }
-        return accepted;
+                circle = new Circle();
+                elemento = new Text();
+                circle.setFill(Color.rgb(255,0,0,.99));
+                circle.setCenterX(Circlex);
+                circle.setCenterY(Circley);
+                Circlex = Circlex + 80;                
+                Circley = Circley + 80;
+                elemento.setText("q1");
+                //elemento.setFill(Color.rgb(255,255,255,.99));
+                elemento.setFill(Color.rgb(68,68,68)); 
+                circle.toFront();
+                elemento.toFront();
+                elemento.setX(circle.getCenterX()-2);
+                elemento.setY(circle.getCenterY()+5);
+                group = new Group(circle, elemento);
+                hbox.getChildren().clear();
+                hbox.getChildren().addAll(vbox,vbox2, button3);
+                pane.getChildren().addAll(group, hbox);
+    
+				}
+		});
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    
+       
     }
-    */
-}
-}
-   
+
+}  
--- Ndfa.java
+++ Ndfa.java
@@ -1,42 +1,123 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
-import java.util.HashMap;
-
+import javax.sound.midi.Receiver;
+import java.util.*;
+/**
+ * 
+ * @param <T>
+ */
 
 public class Ndfa<T extends Comparable>{
-    //Ver su puedo hacer una lista con transitions
-
-    public HashMap<String,String> transitions = new HashMap<String, String>();
-    public HashMap<String,String> transitions1 = new HashMap<String, String>();
-    public HashMap<String,String> transitions2 = new HashMap<String, String>();
-    public List<T> trans;
-    public HashMap<String, HashMap<String,String>> transitionTable = new HashMap<String, HashMap<String,String>>();
-
+    public HashMap<String, HashMap<String,ArrayList<String>>> transitionTable = new HashMap<String, HashMap<String,ArrayList<String>>>();
+    String chara;
+    String temp;
+    int len, i , j;
+    ArrayList<String> reachable = new ArrayList<>();
+    ArrayList<String> tempe = new ArrayList<>();
+    public ArrayList<String> previous = new ArrayList<>();
+    /**
+     * 
+     */
     public Ndfa(){
         
     }
-
-    public void addState(String llave){
-        if(llave == "q0"){
-            transitionTable.put(llave, transitions);
+    /**
+     * This method adds the states as a key of the HashMap
+     * @param state The name of the state 
+     * 
+     */
+    public void addState(String state){
+        transitionTable.put(state, new HashMap<String, ArrayList<String>>());
+    }
+    /**
+     * This method adds the transactions that can happen in a single state
+     * Use another HashMap to add those transactions
+     * The symbol is the Key of this new HashMap
+     * 
+     * @param state1 The key of the first HashMap, the initial state of the transaction
+     * @param symbol The char which make the transaction posible
+     * @param state2 The final state after the transaction
+     * 
+     */
+    public void addTrans(String state1,String symbol , String state2 ){
+        if(transitionTable.get(state1).get(symbol) == null){
+            transitionTable.get(state1).put(symbol, new ArrayList<>());
         }
-        else if(llave == "q1"){
-            transitionTable.put(llave, transitions1);
+        transitionTable.get(state1).get(symbol).add(state2);
+    }
+    
+    /**
+     * This method analyzes the string
+     * @param str The string that will be analys 
+     * @param ini The initial state of the NDFA
+     * @return All the reachable states when proccessing the whole string 
+     * that will be analys
+     */
+    public ArrayList stringProcessing(String str, String ini){
+        previous = new ArrayList<String>();
+        previous.add(ini);
+        if(str.length()==0){
+            reachable.addAll(transitionTable.get(previous.get(0)).get("lmd"));
+            return reachable;
+        }
+        else{
+            for(i = 0; i < str.length();i++){
+                
+                chara = String.valueOf(str.charAt(i)) ;
+                reachable = new ArrayList<String>();
+                //System.out.println("Processing lambda");
+                for(int k = 0; k < previous.size();k++){
+                    reachable.addAll(transitionTable.get(previous.get(k)).get("lmd"));
+                    
+                }
+                //System.out.println(reachable);
+                //System.out.println("Processing " + str.charAt(i));
+                previous= reachable;
+                reachable = new ArrayList<String>();
+                for(j = 0; j < previous.size();j++){
+                    if(transitionTable.get(previous.get(j)).containsKey(chara)){
+                        reachable.addAll(transitionTable.get(previous.get(j)).get(chara));
+                        
+                    }
+                    else{
+                        reachable.clear();
+                    }
+                    
+                }
+                //System.out.println(reachable);
+                previous= reachable;    
+                reachable = new ArrayList<String>();
+                //System.out.println("Processing lambda");
+                for(int k = 0; k < previous.size();k++){
+                    reachable.addAll(transitionTable.get(previous.get(k)).get("lmd"));
+                    
+                }
+                //System.out.println(reachable);
+                previous = reachable;
+                //System.out.println("reachable");
+                //System.out.println(reachable);
+            }
+            if(reachable.size()>1){
+                return reachable;
+            }
+            else{
+                //System.out.println("Processing lambda");
+                previous = reachable;
+                reachable = new ArrayList<>();
+                if(previous.size() > 0){
+                    //System.out.println(reachable + "holi");
+                    for(int k = 0; k < previous.size();k++){
+                        reachable.addAll(transitionTable.get(previous.get(k)).get("lmd"));
+                    }
+            }
+            }
+            
         }
         
+        
+        return reachable;
     }
-
-    public void addTrans(String state1,String symbol , String state2 ){
-        //Cada uno tiene un array de transitions
-        if(state1 == "q0"){
-            transitions.put(symbol, state2);
-            transitionTable.put(state1, transitions);
-        }
-        else if(state1 == "q1"){
-            transitions1.put(symbol, state2);
-            transitionTable.put(state1, transitions1);
-        }
-        System.out.println(transitionTable);
-    }
-
-    
+        
 }
+
